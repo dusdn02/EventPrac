d5a11334f9f4dda0cc74a7bb29d4e7b8b271f2fe
dusdn02 <lyw9114@naver.com>
2020-06-03 14:11:21 +0900

    초기 커밋.

---
+++ EventPrac/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventPrac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Text = "으악";
+            btn.Enabled = false;
+            //textBox1.Text += "♬";
+            //label1.Text += "♬";
+        }
+    }
+}
